47ca90221cbc3316b675cbed5599429b23829d60
nlee1303 <58080121+nlee1303@users.noreply.github.com>
2020-01-02 10:43:35 -0500

    created game chips

--- code.py
+++ code.py
@@ -36,3 +36,34 @@ for n in range(10):
     pygame.draw.rect(win, white, (x,y,width,height))
 
 pygame.display.flip()
+
+
+blue = (0,0,255)
+red = (255,0,0)
+green = (0,255,0)
+purple = (255,0,255)
+
+# chip_color = input('''Choose your color: blue, red, green, yellow
+# ''')
+
+# if chip_color is blue:
+x = 0
+y = 0
+chip = pygame.draw.rect(win, blue, (x,y,5,5))
+
+# if chip_color is red:
+x = 10
+y = 0
+chip = pygame.draw.rect(win, red, (x,y,5,5))
+
+# if chip_color is green:
+x = 0
+y = 10
+chip = pygame.draw.rect(win, green, (x,y,5,5))
+
+# if chip_color is purple:
+x = 10
+y = 10
+chip = pygame.draw.rect(win, purple, (x,y,5,5))
+
+pygame.display.flip()
