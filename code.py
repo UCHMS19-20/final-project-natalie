a2a818c47cbcf8695c7c35f5f6fcdbdd774cc974
nlee1303 <58080121+nlee1303@users.noreply.github.com>
2020-01-14 10:06:15 -0500

    halfed the size, now double original size

--- code.py
+++ code.py
@@ -17,75 +17,77 @@ purple = (255,0,255)
 gold = (212,175,55)
 
 # create window
-win = pygame.display.set_mode((1000,1000))
+win = pygame.display.set_mode((500,500))
 win.fill (black)
 
-# define width and height variables
-width = 80
-height = 80
+def draw_board():
+    width = 40
+    height = 40
+    # draw top row
+    x = -50
+    y = 0
+    for n in range(10):
+        x += 50
+        pygame.draw.rect(win, white, (x,y,width,height))
 
-# draw top row
-x = -100
-y = 0
-for n in range(10):
-    x += 100
-    pygame.draw.rect(win, white, (x,y,width,height))
+    # adds special spaces for top row
+    x = -100
+    y = 0
+    for n in range(2):
+        x += 200
+        pygame.draw.rect(win, gold, (x,y,width,height))
 
-# adds special spaces for top row
-x = -200
-y = 0
-for n in range(2):
-    x += 400
-    pygame.draw.rect(win, gold, (x,y,width,height))
+    # draw bottom row
+    x = -50
+    y = 450
+    for n in range(10):
+        x += 50
+        pygame.draw.rect(win, white, (x,y,width,height))
 
-# draw bottom row
-x = -100
-y = 900
-for n in range(10):
-    x += 100
-    pygame.draw.rect(win, white, (x,y,width,height))
+    # adds special spaces for bottom row
+    x = -100
+    y = 450
+    for n in range(2):
+        x += 250
+        pygame.draw.rect(win, gold, (x,y,width,height))
 
-# adds special spaces for bottom row
-x = -200
-y = 900
-for n in range(2):
-    x += 500
-    pygame.draw.rect(win, gold, (x,y,width,height))
+    # draw left column
+    x = 0
+    y = -50
+    for n in range(10):
+        y += 50
+        pygame.draw.rect(win, white, (x,y,width,height))
 
-# draw left column
-x = 0
-y = -100
-for n in range(10):
-    y += 100
-    pygame.draw.rect(win, white, (x,y,width,height))
+    # adds special spaces for left column
+    x = 0
+    y = -50
+    for n in range(3):
+        y += 150
+        pygame.draw.rect(win, gold, (x,y,width,height))
 
-# adds special spaces for left column
-x = 0
-y = -100
-for n in range(3):
-    y += 300
-    pygame.draw.rect(win, gold, (x,y,width,height))
+    # draw right column
+    x = 450
+    y = -50
+    for n in range(10):
+        y += 50
+        pygame.draw.rect(win, white, (x,y,width,height))
 
-# draw right column
-x = 900
-y = -100
-for n in range(10):
-    y += 100
-    pygame.draw.rect(win, white, (x,y,width,height))
+    # adds special spaces for right column
+    x = 450
+    y = -100
+    for n in range(3):
+        y += 150
+        pygame.draw.rect(win, gold, (x,y,width,height))
+    return
 
-# adds special spaces for right column
-x = 900
-y = -200
-for n in range(3):
-    y += 300
-    pygame.draw.rect(win, gold, (x,y,width,height))
+draw_board()
 
 # print title of game
-font = pygame.font.SysFont('Arial', 200)
+font = pygame.font.SysFont('Arial', 100)
 text1 = font.render('Gold', True, gold)
 text2 = font.render('Rush', True, gold)
-win.blit(text1, (260,240))
-win.blit(text2, (260,480))
+win.blit(text1, (130,120))
+win.blit(text2, (130,240))
 
 # update display
 pygame.display.flip()
@@ -93,7 +95,7 @@ pygame.display.flip()
 # wait 3 seconds
 pygame.time.delay(3000)
 # "erases" title
-win.fill (black,(200,200,600,600))
+win.fill (black,(100,100,300,300))
 
 # update display
 pygame.display.flip()
@@ -106,31 +108,25 @@ p1 = {
 
 # player 2
 p2 = {
-    'x': 40,
+    'x': 20,
     'y': 0
 }
 
 # player 3
 p3 = {
     'x': 0,
-    'y': 40
+    'y': 20
 }
 
 # player 4
 p4 = {
-    'x': 40,
-    'y': 40
+    'x': 20,
+    'y': 20
 }
 
 # function for number of players
 def player_num():
-    # asks user for number of players
-    num = int(input('''How many players are playing? 2 to 4 players
-    '''))
-    # validates user answer
-    while num < 2 or num > 4:
-        num = int(input('''You need 2 to 4 players to play this game. Try again
-        '''))
+    pygame.draw.rect(win, gold, (x,y,width,height))
     return num
 
 # get number of players
@@ -138,19 +134,19 @@ num = player_num()
 
 # display chips for 2 players
 if num is 2:
-    pygame.draw.rect(win, blue, (p1["x"],p1["y"],20,20))
-    pygame.draw.rect(win, red, (p2["x"],p2["y"],20,20))
+    pygame.draw.rect(win, blue, (p1["x"],p1["y"],10,10))
+    pygame.draw.rect(win, red, (p2["x"],p2["y"],10,10))
 # display chips for 3 players
 if num is 3:
-    pygame.draw.rect(win, blue, (p1["x"],p1["y"],20,20))
-    pygame.draw.rect(win, red, (p2["x"],p2["y"],20,20))
-    pygame.draw.rect(win, green, (p3["x"],p3["y"],20,20))
+    pygame.draw.rect(win, blue, (p1["x"],p1["y"],10,10))
+    pygame.draw.rect(win, red, (p2["x"],p2["y"],10,10))
+    pygame.draw.rect(win, green, (p3["x"],p3["y"],10,10))
 # display chips for 4 players
 if num is 4:
-    pygame.draw.rect(win, blue, (p1["x"],p1["y"],20,20))
-    pygame.draw.rect(win, red, (p2["x"],p2["y"],20,20))
-    pygame.draw.rect(win, green, (p3["x"],p3["y"],20,20))
-    pygame.draw.rect(win, purple, (p4["x"],p4["y"],20,20))
+    pygame.draw.rect(win, blue, (p1["x"],p1["y"],10,10))
+    pygame.draw.rect(win, red, (p2["x"],p2["y"],10,10))
+    pygame.draw.rect(win, green, (p3["x"],p3["y"],10,10))
+    pygame.draw.rect(win, purple, (p4["x"],p4["y"],10,10))
 
 # update display
 pygame.display.flip()
@@ -172,7 +168,7 @@ while rounds < 11:
     # random number between 1 and 6
     dice1 = random.randint(1,6)
     print(f'You rolled a {dice}')
-    p1['x'] += (dice*80)
+    p1['x'] += (dice*40)
     
     # wait 1 second
     pygame.time.delay(1000)
@@ -183,7 +179,7 @@ while rounds < 11:
     # random number between 1 and 6
     dice = random.randint(1,6)
     print(f'You rolled a {dice}')
-    p2['x'] += (dice*80)
+    p2['x'] += (dice*40)
 
     if num is 3 or 4:
         # wait 1 second
@@ -194,7 +190,7 @@ while rounds < 11:
         # random number between 1 and 6
         dice = random.randint(1,6)
         print(f'You rolled a {dice}')
-        p3['x'] += (dice*80)
+        p3['x'] += (dice*40)
 
     if num is 4:
         # wait 1 second
@@ -205,7 +201,7 @@ while rounds < 11:
         # random number between 1 and 6
         dice = random.randint(1,6)
         print(f'You rolled a {dice}')
-        p4['x'] += (dice*80)
+        p4['x'] += (dice*40)
 
     # update display
     pygame.display.flip()
