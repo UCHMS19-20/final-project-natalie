91e83f0b8a7a1b3538084dc78f046c2f3a74ce8d
nlee1303 <58080121+nlee1303@users.noreply.github.com>
2020-01-16 10:51:17 -0500

    finished player function and modified event loop

--- code.py
+++ code.py
@@ -21,11 +21,8 @@ def wait(sec):
     pygame.time.delay(sec*1000)
     return
 
-# create window
-win = pygame.display.set_mode((500,500))
-win.fill (black)
-
-def draw_board():
+# defined board
+def draw_board(self):
     width = 40
     height = 40
     # draw top row
@@ -85,143 +82,157 @@ def draw_board():
         pygame.draw.rect(win, gold, (x,y,width,height))
     return
 
-draw_board()
-
-# print title of game
-font = pygame.font.SysFont('Arial', 100)
-text = font.render('Gold', True, gold)
-win.blit(text, (130,120))
-text = font.render('Rush', True, gold)
-win.blit(text, (130,240))
-
-# update display
-pygame.display.flip()
-
-# wait 3 seconds
-wait(3)
-# "erases" title
-win.fill (black,(100,100,300,300))
-
-# update display
-pygame.display.flip()
+# defined title of game
+def title(self):
+    font = pygame.font.SysFont('Arial', 100)
+    text = font.render('Gold', True, gold)
+    win.blit(text, (130,120))
+    text = font.render('Rush', True, gold)
+    win.blit(text, (130,240))
+    return
 
 # player 1
 p1 = {
     'x': 0,
-    'y': 0
+    'y': 0,
+    '$': 0
 }
 
 # player 2
 p2 = {
     'x': 20,
-    'y': 0
+    'y': 0,
+    '$': 0
 }
 
 # player 3
 p3 = {
     'x': 0,
-    'y': 20
+    'y': 20,
+    '$': 0
 }
 
 # player 4
 p4 = {
     'x': 20,
-    'y': 20
+    'y': 20,
+    '$': 0
 }
 
 # function for number of players
-def player_num():
+def player_num(self):
     font = pygame.font.SysFont('Arial', 20)
 
     pygame.draw.rect(win, blue, (170,140,160,80))
-    two_p = pygame.draw.rect(win, gold, (175,145,150,70))
+    two_p = pygame.Rect(175,145,150,70)
+    pygame.draw.rect(win, gold, two_p)
     text = font.render('2 players', True, black)
     win.blit(text, (210,170))
 
     pygame.draw.rect(win, blue, (70,260,160,80))
-    three_p = pygame.draw.rect(win, gold, (75,265,150,70))
+    three_p = pygame.Rect(75,265,150,70)
+    pygame.draw.rect(win, gold, three_p)
     text = font.render('3 players', True, black)
     win.blit(text, (110,290))
 
     pygame.draw.rect(win, blue, (270,260,160,80))
-    four_p = pygame.draw.rect(win, gold, (275,265,150,70))
+    four_p = pygame.Rect(275,265,150,70)
+    pygame.draw.rect(win, gold, four_p)
     text = font.render('4 players', True, black)
     win.blit(text, (310,290))
 
-    if pygame.mouse.get_pressed() on two_p:
-        pygame.draw.rect(win, blue, (p1["x"],p1["y"],10,10))
-        pygame.draw.rect(win, red, (p2["x"],p2["y"],10,10))
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if two_p.collidepoint(pygame.mouse.get_pos()[0]):
+            pygame.draw.rect(win, blue, (p1["x"],p1["y"],10,10))
+            pygame.draw.rect(win, red, (p2["x"],p2["y"],10,10))
 
-    if pygame.mouse.get_pressed() on three_p:
-        pygame.draw.rect(win, blue, (p1["x"],p1["y"],10,10))
-        pygame.draw.rect(win, red, (p2["x"],p2["y"],10,10))
-        pygame.draw.rect(win, green, (p3["x"],p3["y"],10,10))
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if three_p.collidepoint(pygame.mouse.get_pos()[0]):
+            pygame.draw.rect(win, blue, (p1["x"],p1["y"],10,10))
+            pygame.draw.rect(win, red, (p2["x"],p2["y"],10,10))
+            pygame.draw.rect(win, green, (p3["x"],p3["y"],10,10))
     
-    if pygame.mouse.get_pressed() on four_p:
-        pygame.draw.rect(win, blue, (p1["x"],p1["y"],10,10))
-        pygame.draw.rect(win, red, (p2["x"],p2["y"],10,10))
-        pygame.draw.rect(win, green, (p3["x"],p3["y"],10,10))
-        pygame.draw.rect(win, purple, (p4["x"],p4["y"],10,10)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+        if four_p.collidepoint(pygame.mouse.get_pos()[0]):
+            pygame.draw.rect(win, blue, (p1["x"],p1["y"],10,10))
+            pygame.draw.rect(win, red, (p2["x"],p2["y"],10,10))
+            pygame.draw.rect(win, green, (p3["x"],p3["y"],10,10))
+            pygame.draw.rect(win, purple, (p4["x"],p4["y"],10,10))
+    return
 
-
-# get number of players
-player_num()
-
-# update display
-pygame.display.flip()
-
-# wait 2 seconds
-wait(2)
-
-# main loop
 while True:
-    # do something for each event in the event queue
     for event in pygame.event.get():
-        # check to see if the current event is a QUIT event
         if event.type == pygame.QUIT:
-            # exit the program
             sys.exit()
+        elif pygame.mouse.get_pressed():
+            main = True
 
-    print("It's player 1's turn")
-    # wait 1 second
-    wait(1)
-    # random number between 1 and 6
-    dice = random.randint(1,6)
-    print(f'You rolled a {dice}')
-    p1['x'] += (dice*40)
-    
-    # wait 1 second
-    wait(1)
 
-    print("It's player 2's turn")
-    # wait 1 second
-    wait(1)
-    # random number between 1 and 6
-    dice = random.randint(1,6)
-    print(f'You rolled a {dice}')
-    p2['x'] += (dice*40)
+    intro = True
+    while intro:
+        # create window
+        win = pygame.display.set_mode((500,500))
+        win.fill (black)
+        draw_board()
+        title()
+        # update display
+        pygame.display.flip()
+        # wait 3 seconds
+        wait(3)
+        # "erases" title
+        win.fill (black,(100,100,300,300))
+        # update display
+        pygame.display.flip()
+        # get number of players
+        player_num()
+    # wait 2 seconds
+    wait(2)
 
-    if num is 3 or 4:
-        # wait 1 second
-        wait(1)
-        print("It's player 3's turn")
-        # wait 1 second
-        wait(1)
-        # random number between 1 and 6
-        dice = random.randint(1,6)
-        print(f'You rolled a {dice}')
-        p3['x'] += (dice*40)
-
-    if num is 4:
-        # wait 1 second
-        wait(1)
-        print("It's player 4's turn")
+    while main:
+        print("It's player 1's turn")
         # wait 1 second
         wait(1)
         # random number between 1 and 6
         dice = random.randint(1,6)
         print(f'You rolled a {dice}')
-        p4['x'] += (dice*40)
+        p1['x'] += (dice*40)
+        # update display
+        pygame.display.flip()
 
-    # update display
-    pygame.display.flip()
+        # wait 1 second
+        wait(1)
+        print("It's player 2's turn")
+        # wait 1 second
+        wait(1)
+        # random number between 1 and 6
+        dice = random.randint(1,6)
+        print(f'You rolled a {dice}')
+        p2['x'] += (dice*40)
+        # update display
+        pygame.display.flip()
+
+        if num is 3 or 4:
+            # wait 1 second
+            wait(1)
+            print("It's player 3's turn")
+            # wait 1 second
+            wait(1)
+            # random number between 1 and 6
+            dice = random.randint(1,6)
+            print(f'You rolled a {dice}')
+            p3['x'] += (dice*40)
+            # update display
+            pygame.display.flip()
+
+        if num is 4:
+            # wait 1 second
+            wait(1)
+            print("It's player 4's turn")
+            # wait 1 second
+            wait(1)
+            # random number between 1 and 6
+            dice = random.randint(1,6)
+            print(f'You rolled a {dice}')
+            p4['x'] += (dice*40)
+            # update display
+            pygame.display.flip()
